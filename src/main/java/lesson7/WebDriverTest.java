d9d85d3c06b2ed9d0c18e5a94f8014348ae8164a
Oleg Novitskiy <oleg.novitskiy.dev@gmail.com>
2018-07-14 00:32:17 +0300

    Add "Today's featured picture" test

--- src/main/java/lesson7/WebDriverTest.java
+++ src/main/java/lesson7/WebDriverTest.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.support.ui.Select;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class WebDriverTest extends BaseTest {
 
@@ -34,5 +35,18 @@ public class WebDriverTest extends BaseTest {
                 "Wrong title");
 
         assertEquals(searchString, driver.findElement(By.xpath("//*[@id=\"mw-content-text\"]/div/table[1]/tbody/tr[1]/th/span")).getText(), "Wrong title");
+    }
+
+    @Test
+    public void todaysFeaturedPictureNotEmpty() {
+        driver.get("https://www.wikipedia.org/");
+
+        driver.findElement(By.xpath("//*[@id=\"js-link-box-en\"]")).click();
+
+        String featuredPictureText = "Today's featured picture";
+        WebElement featuredPictureBlock = driver.findElement(By.id("mp-bottom"));
+
+        assertEquals(featuredPictureText, featuredPictureBlock.findElement(By.id("Today's_featured_picture")).getText(), "Wrong title");
+        assertFalse(featuredPictureBlock.findElement(By.xpath("//*[@id=\"mp-tfp\"]/table/tbody/tr/td[1]/a/img")).getAttribute("src").isEmpty(), "A picture missing");
     }
 }
