ea911f6985165777fb3cc3ed7d8c1d9a36aca2da
Oleg Novitskiy <oleg.novitskiy.dev@gmail.com>
2018-07-13 23:48:32 +0300

    Task 2

--- src/main/java/lesson7/WebDriverTest.java
+++ src/main/java/lesson7/WebDriverTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
@@ -17,14 +18,21 @@ public class WebDriverTest extends BaseTest {
     public void test(){
         driver.get("https://www.wikipedia.org/");
 
-        WebElement searchInput = driver.findElement(By.xpath("//*[@id='searchInput']"));
         String searchString = "Ukraine";
+        WebElement searchInput = driver.findElement(By.xpath("//*[@id='searchInput']"));
         searchInput.sendKeys(searchString);
+
+        String currentLanguage = "English";
+        Select drpCountry = new Select(driver.findElement(By.id("searchLanguage")));
+        drpCountry.selectByVisibleText(currentLanguage);
+
+        assertEquals(currentLanguage, drpCountry.getFirstSelectedOption().getText(), "Wrong language");
 
         driver.findElement(By.cssSelector("[type='submit']")).click();
 
         assertEquals(searchString, driver.findElement(By.id("firstHeading")).getText(),
                 "Wrong title");
 
+        assertEquals(searchString, driver.findElement(By.xpath("//*[@id=\"mw-content-text\"]/div/table[1]/tbody/tr[1]/th/span")).getText(), "Wrong title");
     }
 }
