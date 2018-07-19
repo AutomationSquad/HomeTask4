800218f687c99dbb5217f7afef3461e52fc9709b
ihorshcherbyna <ihorshcherbyna.ck@gmail.com>
2018-07-19 22:51:14 +0300

    Implemented tests: invalid login, feature day image, Ukraine search

--- src/main/java/lesson7/WebDriverTest.java
+++ src/main/java/lesson7/WebDriverTest.java
@@ -2,20 +2,21 @@ package lesson7;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WebDriverTest extends BaseTest {
 
     @Test
-    public void test(){
+    public void test() {
         driver.get("https://www.wikipedia.org/");
+
+        Select language = new Select(driver.findElement(By.id("searchLanguage")));
+        language.selectByVisibleText("English");
+
 
         WebElement searchInput = driver.findElement(By.xpath("//*[@id='searchInput']"));
         String searchString = "Ukraine";
@@ -25,6 +26,8 @@ public class WebDriverTest extends BaseTest {
 
         assertEquals(searchString, driver.findElement(By.id("firstHeading")).getText(),
                 "Wrong title");
+        assertEquals(searchString, driver.findElement(By.cssSelector("[class='fn org country-name']")).getText(),
+                "Wrong country is opened");
 
     }
 }
